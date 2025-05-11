715634490a9f7fc3d4845f75606f3963f73550e0
Benlamir <benlamirate01@gmail.com>
2025-05-11 22:47:35 +0100

    ws url for staging env

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 // frontend/js/config.js
 export const WEBSOCKET_URL =
-  "wss://bs8e4l0vld.execute-api.us-east-1.amazonaws.com/dev";
+  "wss://bs8e4l0vld.execute-api.us-east-1.amazonaws.com/staging";
 
 // We can add other config like Resonator data source here later
